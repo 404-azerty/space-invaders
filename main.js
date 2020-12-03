8214c1071f85f62e2307d4c7ec83ae3ffe0cfdf7
Johanne <johannemay@icloud.com>
2020-12-03 15:57:45 +0100

    déplacements des personnages

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const container = document.querySelector(`.grid`);
 let allDivs;
 let aliens = [];
 let unicornPosition = 229;
+let alienDirection = 1;
 
 function createGridAndAliens() {
   let indexAttr = 0;
@@ -41,3 +42,59 @@ function createGridAndAliens() {
   allDivs[unicornPosition].classList.add(`unicorn`);
 }
 createGridAndAliens();
+
+function moveUnicorn(event) {
+  allDivs[unicornPosition].classList.remove(`unicorn`);
+  // gestion du déplacement de la licorne
+  // if (event.keyCode === 37) {
+  //   if (unicornPosition > 220) {
+  //     unicornPosition -= 1;
+  //   }
+  // } else if (event.keyCode === 39) {
+  //   if (unicornPosition < 239) {
+  //     unicornPosition += 1;
+  //   }
+  // }
+  if (event.keyCode === 37 && unicornPosition > 220) {
+    unicornPosition -= 1;
+  } else if (event.keyCode === 39 && unicornPosition < 239) {
+    unicornPosition += 1;
+  }
+  allDivs[unicornPosition].classList.add(`unicorn`);
+}
+document.addEventListener(`keydown`, moveUnicorn);
+
+// mouvements des aliens
+let moveToBottomRight = true;
+let moveToBottomLeft = true;
+function moveAliens() {
+  for (let index = 0; index < aliens.length; index++) {
+    if (allDivs[aliens[index]].getAttribute(`data-right`) === `true`) {
+      if (moveToBottomRight) {
+        alienDirection = 20;
+        setTimeout(() => (moveToBottomRight = false), 50);
+      } else if (!moveToBottomRight) {
+        alienDirection = -1;
+      }
+      moveToBottomLeft = true;
+    } else if (allDivs[aliens[index]].getAttribute(`data-left`) === `true`) {
+      if (moveToBottomLeft) {
+        alienDirection = 20;
+        setTimeout(() => (moveToBottomLeft = false), 50);
+      } else if (!moveToBottomLeft) {
+        alienDirection = 1;
+      }
+      moveToBottomRight = true;
+    }
+  }
+  for (let index = 0; index < aliens.length; index++) {
+    allDivs[aliens[index]].classList.remove(`alien`);
+  }
+  for (let index = 0; index < aliens.length; index++) {
+    aliens[index] += alienDirection;
+  }
+  for (let index = 0; index < aliens.length; index++) {
+    allDivs[aliens[index]].classList.add(`alien`);
+  }
+}
+movingAliens = setInterval(() => moveAliens(), 500);
